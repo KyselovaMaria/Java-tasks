0a1cdf67a7456a7642c6143f43cd5e7417ec3ce2
KyselovaMaria <88087036+KyselovaMaria@users.noreply.github.com>
2022-11-03 16:41:10 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        Neurosurgeon neurosurgeon = new Neurosurgeon(10, "Bogomolca");
-        neurosurgeon.getwork_experience();
-        System.out.println("Education: " + neurosurgeon.geteducation());
-
-    }
-}
